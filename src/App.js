6fbcda63b5e67edcb55d22946bc0868a25e9e35f
Ndumiso Thela <ndumisothela480@gmail.com>
2022-09-08 03:50:21 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
